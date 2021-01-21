d48f8bc9df6d9d56a9009beb0225e3e4b7ae1359
sangkipark <sangki931@gmail.com>
2021-01-21 12:34:10 +0900

    Update my code

--- morsecode.py
+++ morsecode.py
@@ -278,12 +278,12 @@ def encoding_sentence(english_sentence):
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     result=''
     for i in english_sentence:
-        if i==' ':
+        if i in [' ','.',',','!','?']:
             result+=' '
         else:
             result+=encoding_character(i)
 
-    return result
+    return result.rstrip()
     # ==================================
 
 
